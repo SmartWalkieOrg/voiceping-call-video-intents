f018e947f7c2939a5e711deb2dcd51c71396609e
Aditya Wibisana <aditya.wibisana@gmail.com>
2021-12-21 15:52:32 +0700

    Receive intent for incoming call

--- app/src/main/java/com/smartwalkietalkie/gantry/MainActivity.kt
+++ app/src/main/java/com/smartwalkietalkie/gantry/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.smartwalkietalkie.gantry
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val statusTextView = findViewById<TextView>(R.id.status)
+
+        val filter = IntentFilter()
+        filter.addAction("INCOMING_CALL")
+        val updateUIReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val userId = intent.getIntExtra(EXTRAS_USER_ID, -1)
+                statusTextView.text = "Incoming call from $userId"
+            }
+        }
+        registerReceiver(updateUIReceiver, filter)
+    }
+
+    companion object {
+        const val EXTRAS_USER_ID = "user_id"
     }
 }
